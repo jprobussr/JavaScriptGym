baf2df6b8d2d9e9ed010812a0f707b5ad78c1dc9
John <johnprobussr@gmail.com>
2024-12-11 10:13:53 -0600

    add compare function

--- main.js
+++ main.js
@@ -30,6 +30,26 @@ const pAequorFactory = (specimenNum, dna) => {
             const newBase = newBases[Math.floor(Math.random() * newBases.length)];
             this.dna[randomIndex] = newBase;
             return this.dna;
-        }
-    }
-}
+        },
+
+        compareDNA(otherPAequor) {
+            let commonCount = 0;
+            // Compare DNA bases at each index
+            this.dna.forEach((base, index) => {
+                if (base === otherPAequor.dna[index]) {
+                    commonCount++;
+                }
+            });
+
+            // Calculate percentage of identical bases
+            const percentage = ((commonCount / this.dna.length));
+
+            console.log(`Specimen #${this.specimenNum} and Specimen #${otherPAequor.specimenNum} have ${percentage}% DNA in common.`);
+        },
+    };
+};
+
+const pAequor1 = pAequorFactory(1, ['A', 'C', 'T', 'G', 'A', 'C', 'T', 'G', 'A', 'C', 'T', 'G', 'A', 'C', 'T']);
+const pAequor2 = pAequorFactory(2, ['A', 'C', 'T', 'G', 'C', 'A', 'T', 'G', 'A', 'C', 'T', 'G', 'A', 'C', 'T']);
+
+pAequor1.compareDNA(pAequor2);
