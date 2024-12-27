a131e2ee38f5f8f7dc8522cb7c13dfe2b025adf8
John <johnprobussr@gmail.com>
2024-12-27 10:20:24 -0600

    practice singlyLinked List, classes, and frequency counter function

--- randomJS/singleLinkList.js
+++ randomJS/singleLinkList.js
@@ -1,47 +1,40 @@
+// Define a Node 
 class Node {
-    constructor(data) {
-        this.data = data; //The value stored in the node
-        this.next = null; // Pointer to the next node, initially null
+    constructor(value) {
+        this.value = value;
+        this.next = null; // Pointer to the next node;
     }
 }
 
-// SinglyLinkedList class to manage the list
-
+// Define the singly Linked List
 class SinglyLinkedList {
     constructor() {
-        this.head = null; // Points to the first node in the list
-        this.tail = null; // Points to the last node in the list
-        this.length = 0; // Number of nodes in the list
+        this.head = null; // Start the list
+        this.size = 0; // Track the size of the list
     }
 
-    // Method to add new node to the end of the list
-    push(data) {
-        let newNode = new Node(data); //Create a new Node with the given data
+    // Add a new node to the end of the list
+    add(value) {
+        const newNode = new Node(value);
         if (!this.head) {
-            // If the list is empty
-            this.head = newNode; // Set head to the new node
-            this.tail = this.head // Tail also points to the same node
+            this.head = newNode; // If the list is empty, set head to new node;
         } else {
-            // If the list already has nodes
-            this.tail.next = newNode; // Link the current tail to the new node
-            this.tail = newNode; // Update the tail to be the new node;
+            let current = this.head;
+            while (current.next) {
+                current = current.next; // Traverse to the end of the list
+            }
+            current.next = newNode; // Link the last node to the new node
         }
-        this.length++; // Increment the length of the list
-        return this; // Return the entire list for chaining
+        this.size++;
     }
 }
 
-// Create a  new SinglyLinkedList instance
-let list = new SinglyLinkedList();
+const list = new SinglyLinkedList();
 
-list.push('Hello'); // Add node with the value 'Hello'
-list.push('How Are You?'); //Add node
-list.push(99); // Add node
+console.log("Adding 10...");
+list.add(10);
+console.log("Head Node:", list.head); // Logs the head Node.
 
-// Log the entire list
-console.log('Entire List:', list);
-
-// Log specific parts of the list
-console.log('Head Node:', list.head); // Log the head node
-console.log('Second Node:', list.head.next); // Log the second node
-console.log('Third Node:', list.head.next.next); // Log the third node
+console.log('Adding 20...');
+list.add(20);
+console.log('Head Node:', list.head); // Logs the head node and its next node.
