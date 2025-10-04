5717c6aac7fab51bc39ef3056a6d116ded30626b
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-10-03 19:00:21 -0500

    Add Weather to Dashboard.

--- PersonalDashBoard/app.js
+++ PersonalDashBoard/app.js
@@ -44,14 +44,29 @@ fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
 
 const getCurrentTime = () => {
   const date = new Date();
-  document.getElementById('time').textContent = date.toLocaleTimeString('en-us', {
-    timeStyle: 'medium'
-  })
+  document.getElementById('time').textContent = date.toLocaleTimeString(
+    'en-us',
+    {
+      timeStyle: 'medium',
+    }
+  );
 };
 
-setInterval(getCurrentTime, 1000)
+setInterval(getCurrentTime, 1000);
 
-
-navigator.geolocation.getCurrentPosition(position => {
-  console.log(position)
-})
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error('Weather data is not available.');
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const iconUrl = `httpp://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      console.log(iconUrl)
+    })
+    .catch((err) => console.error(err));
+});
