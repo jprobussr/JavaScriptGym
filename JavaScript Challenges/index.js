db7d11f07fd077bde7e48b15850cead92a68e6c2
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-12-15 18:34:51 -0600

    Add JS Challenges Panic & Whisper functions

---
+++ JavaScript Challenges/index.js
@@ -0,0 +1,19 @@
+
+// Whisper Function
+// const whisper = (str) => {
+//   if (str.endsWith('!')) {
+//     return 'shh... ' + str.slice(0, -1).toLowerCase();
+//   }
+//   return 'shh...' + str.toLowerCase();
+// };
+
+// console.log(whisper('PLEASE BE CAREFUL!'));
+// console.log(whisper('Please GO home!'));
+
+// Panic Function
+
+// const panic = (str) => {
+//   return str.split('').join('🥳').toUpperCase()
+// };
+
+// console.log(panic('hello world part 3'))
