632c29ecf3033d7647fcd8f162d02c1054b89861
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-12-16 18:51:11 -0600

    Work on JS Challenges Emojify toTitleCase

--- JavaScript Challenges/index.js
+++ JavaScript Challenges/index.js
@@ -1,4 +1,3 @@
-
 // Whisper Function
 // const whisper = (str) => {
 //   if (str.endsWith('!')) {
@@ -17,3 +16,87 @@
 // };
 
 // console.log(panic('hello world part 3'))
+
+// Alternating Caps
+
+// const altCaps = str => {
+//     let newStr = '';
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (i % 2 === 0) {
+//             newStr += str[i].toUpperCase();
+//         } else {
+//             newStr += str[i];
+//         }
+//     }
+
+//     return newStr;
+// }
+
+// console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
+
+// To TitleCase()
+
+// const capitalizeWord = word => {
+//     return word[0].toUpperCase() + word.slice(1);
+// }
+
+// const toTitleCase = str => {
+//     const sentenceArr = str.split(' ')
+
+//     const capArr = sentenceArr.map((word) => capitalizeWord(word));
+
+//     return capArr.join(' ');
+// }
+
+// console.log(capitalizeWord('ghost'));
+// console.log(toTitleCase('pumpkin pranced and pranced across the pond, that\'s All!'));
+
+// Not Fiz Buzz
+
+// const awardBonus = () => {
+//     for (let i = 1; i < 100; i++) {
+//         if (i % 15 === 0) {
+//             console.log(`${i} - First Place Prize: Steak Gift Card'`);
+//         } else if (i % 3 === 0) {
+//             console.log(`${i} - Second Place Prize: Amusement Park`)
+//         } else if (i % 5 === 0) {
+//             console.log(`${i} - Third Place Prize: $40 Gift Card`);
+//         }
+//     }
+// }
+
+// awardBonus();
+
+// Emojify
+
+const emojis = {
+  smile: '😊',
+  angry: '😠',
+  party: '🎉',
+  heart: '💜',
+  cat: '🐱',
+  dog: '🐕',
+};
+
+const emojifiedWord = (word) => {
+  if (!word.startsWith(':') || !word.endsWith(':')) {
+    return word;
+  }
+
+  const key = word.slice(1, -1);
+
+  if (emojis[key]) {
+    return emojis[key];
+  }
+
+  return key;
+};
+
+const emojifyPhrase = phrase => {
+    const words = phrase.split(' ');
+    const emojifiedWords = words.map(word => emojifiedWord(word));
+    return emojifiedWords.join(' ');
+}
+console.log(emojifyPhrase("I :heart: my :cat:"));
+console.log(emojifyPhrase("I :heart: my :elephant:"));
