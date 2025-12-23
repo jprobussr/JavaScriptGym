7737eb14e6b05c4742e5ca84c83cb174c43b66cb
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-12-22 18:50:12 -0600

    Practice JS challenges and update Gym

--- JavaScriptChallenges/index.js
+++ JavaScriptChallenges/index.js
@@ -1,17 +1,1 @@
-const altCaps = (str) => {
-  let newStr = '';
-
-  for (let i = 0; i < str.length; i++) {
-    if (i % 2 === 0) {
-        newStr += str[i].toUpperCase();
-    } else {
-        newStr += str[i];
-    }
-  }
-
-  return newStr;
-};
-
-console.log(altCaps('never give up!'));
-console.log(altCaps('I code, therefore I am!'));
-console.log(altCaps('Keep Coding!'));
+// Decode Alien Message Challenge
