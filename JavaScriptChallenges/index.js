3887544221b1f4353ea7dcc0bb5f4d8c42620bdf
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-12-24 13:01:33 -0600

    Work on JS Challenges isPalindrome && Decode Alien Message

--- JavaScriptChallenges/index.js
+++ JavaScriptChallenges/index.js
@@ -1,1 +1,15 @@
-// Decode Alien Message Challenge
+const isPalindrome = str => {
+  let newReverseString = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newReverseString += str[i];
+  }
+
+  return newReverseString === str;
+}
+
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
