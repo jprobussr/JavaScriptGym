a2a4ec3601a389cd8375bac1dfa94b34d7a44fe9
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-12-27 09:11:00 -0600

    Practice writing Node and LinkedList DS

--- LinkedList-1.0/LinkedList.js
+++ LinkedList-1.0/LinkedList.js
@@ -1,2 +1,16 @@
 import Node from './Node.js';
 
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newNode = new Node(data);
+    const currentHead = this.head;
+
+    newNode.setNextNode(currentHead);
+
+    this.head = newNode;
+  }
+}
--- LinkedList-1.0/index.js
+++ LinkedList-1.0/index.js
@@ -2,19 +2,6 @@ import LinkedList from './LinkedList.js';
 
 const list = new LinkedList();
 
-list.addToTail('A');
-list.addToTail('B');
-list.addToTail('C');
-list.printList();
-
-console.log('Removed:', list.removeHead());
-list.printList();
-
-console.log('Removed:', list.removeHead());
-list.printList();
-
-console.log('Removed:', list.removeHead());
-list.printList();
-
-console.log('Removed:', list.removeHead());
-list.printList();
+list.addToHead('Flour');
+list.addToHead('Milk');
+console.log(list);
