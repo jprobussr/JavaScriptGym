897c97fd72639608774b4a9bd3878da8b0d405db
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-01-08 11:33:49 -0600

    Work on JS Challenges Anagrams

--- JavaScriptChallenges/index.js
+++ JavaScriptChallenges/index.js
@@ -1,16 +1,32 @@
-import podcasts from './data.js';
+const anagrams = [
+  'moz biblical torchbearers',
+  "it's razorbill beachcomber",
+  'och mcrobbie trailblazers',
+  'bib chorizo cellarmaster',
+  'thor scribble carbimazole',
+  'zilla borscht abercrombie',
+  'brazil scorcher batmobile',
+  'dame shelburne characterizing',
+  'uber englishman characterized',
+  'agnes rhumbline characterized',
+  'rehab scrutinized charlemagne',
+  'dreams zurich interchangeable',
+  'bam hamster technocratic',
+  'mechatronic masterbatch',
+  'bam ratchet mechatronics',
+];
 
-const createDescription = (data) => {
-  return data.map((podcast) => {
-    const { title, duration, genre, hosts } = podcast;
+const sortPhrase = (phrase) => {
+  return phrase.toLowerCase().split('').sort().join('').trim();
+};
 
-    return {
-      ...podcast,
-      description: `${title} is a ${duration} minute ${genre} podcast hosted 
-            by ${hosts[0]}.`,
-    };
+const isAnagramInArray = (anagram, arr) => {
+  return arr.filter((item) => {
+    const word1 = sortPhrase(anagram);
+    const word2 = sortPhrase(item);
+
+    return word1 === word2;
   });
 };
 
-
-console.log(createDescription(podcasts));
+console.log(isAnagramInArray('Bob Ziroll Scrimba Teacher', anagrams));
