36cfbeac3ec762e05a559369dd4f755017b6b7f4
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-01-24 09:08:57 -0600

    chore: js gym practice with objects and methods

---
+++ ObjectsPractice/app.js
@@ -0,0 +1,34 @@
+let spaceship = {
+  crew: {
+    captain: {
+      name: 'Lily',
+      degree: 'Computer Engineering',
+      cheerTeam() {
+        return 'You got this!';
+      },
+    },
+    'chief officer': {
+      name: 'Dan',
+      degree: 'Aerospace Engineering',
+      agree() {
+        return 'I agree, captain!';
+      },
+    },
+    medic: {
+      name: 'Clementine',
+      degree: 'Physics',
+      announce() {
+        return (`Jets on!`);
+      },
+    },
+    translator: {
+      name: 'Shauna',
+      degree: 'Conservation Science',
+      powerFuel() {
+        console.log('The tank is full!');
+      },
+    },
+  },
+};
+
+console.log(spaceship.crew['chief officer'].agree());
