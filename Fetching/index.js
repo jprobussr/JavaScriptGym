da04de81f6b4afa5630fe5f0684de325fa6f6fc1
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-01-29 13:50:37 -0600

    chore: practice async/await fetch flow with DOM rendering

--- Fetching/index.js
+++ Fetching/index.js
@@ -1,8 +1,38 @@
-fetch('https://apis.scrimba.com/dog.ceo/api/breeds/image/random')
-  .then((response) => response.json())
-  .then((data) => {
-    const imageElement = document.createElement('img');
-    imageElement.src = data.message;
-    imageElement.alt = 'a random dog image';
-    document.getElementById('img-container').appendChild(imageElement);
-  });
+// fetch('https://apis.scrimba.com/dog.ceo/api/breeds/image/random')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const imageElement = document.createElement('img');
+//     imageElement.src = data.message;
+//     imageElement.alt = 'a random dog image';
+//     document.getElementById('img-container').appendChild(imageElement);
+//   });
+
+// const getDogImage = async () => {
+//   const response = await fetch(
+//     'https://apis.scrimba.com/dog.ceo/api/breeds/image/random',
+//   );
+//   const data = await response.json();
+//   const imageElement = document.createElement('img');
+//   imageElement.src = data.message;
+//   imageElement.alt = 'a random dog image';
+//   document.getElementById('img-container').appendChild(imageElement);
+// };
+
+// getDogImage();
+
+const getDogImage = async () => {
+  const response = await fetch(
+    'https://apis.scrimba.com/dog.ceo/api/breeds/image/random',
+  );
+  const data = await response.json();
+  const imageElement = document.createElement('img');
+
+  imageElement.src = data.message;
+  imageElement.alt = 'A random dog image';
+
+  document.getElementById('img-container').appendChild(imageElement);
+
+  console.log(data);
+};
+
+getDogImage();
