52ff7cee3b59505909eadd1c178357ad5854e71d
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-03-14 13:46:45 -0500

    feat: add delete button for each todo item

--- micro-projects/todo-list/script.js
+++ micro-projects/todo-list/script.js
@@ -5,15 +5,24 @@ const taskList = document.getElementById('taskList');
 const taskCount = document.getElementById('taskCount');
 const themeToggle = document.getElementById('themeToggle');
 
-taskForm.addEventListener('click', (e) => {
-  e.preventDefault();
+taskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
   const taskText = taskInput.value;
 
   const li = document.createElement('li');
-  li.textContent = taskText;
+  const taskLabel = document.createElement('span');
+  const deleteBtn = document.createElement('button');
 
+  taskLabel.textContent = taskText;
+  deleteBtn.textContent = 'Delete';
+
+  li.append(taskLabel, deleteBtn);
   taskList.appendChild(li);
 
   taskInput.value = '';
+
+  deleteBtn.addEventListener('click', () => {
+    li.remove();
+  });
 });
